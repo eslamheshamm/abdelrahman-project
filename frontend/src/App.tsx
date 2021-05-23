942c4caaaf4f46b39bc41a474abaaf0a72b1fd9e
eslamheshamm <esl2mhesham@gmail.com>
2021-05-23 15:26:37 +0200

    initalize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+	return (
+		<div>
+			<header className="text-center">
+				<p className="text-4xl uppercase font-mono font-semibold">
+					hello world
+				</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
