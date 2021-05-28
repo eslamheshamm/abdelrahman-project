0921245d85f361ec03c64d9c102a3378bd88abad
eslamheshamm <esl2mhesham@gmail.com>
2021-05-28 18:47:27 +0200

    layots

--- frontend/src/components/layots.tsx
+++ frontend/src/components/layots.tsx
@@ -1,7 +1,16 @@
+import Footer from "./footer";
+import NavBar from "./navbar";
+
 type LayoutsProps = {
 	children: any;
 };
 const Layouts: React.FC<LayoutsProps> = (props) => {
-	return <main className="w-11/12 mx-auto">{props.children}</main>;
+	return (
+		<main className="w-11/12 mx-auto pt-6 pb-16 min-h-screen relative ">
+			<NavBar />
+			<section className="my-12">{props.children}</section>
+			<Footer />
+		</main>
+	);
 };
 export default Layouts;
