e377030d84eed029df85a7026003ade7752a6019
eslamheshamm <esl2mhesham@gmail.com>
2021-06-01 10:39:14 +0200

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,27 +6,20 @@ import Exhibition from "./pages/exhibition";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Layouts from "./components/layots";
+import ProjectPost from "./components/Project/project-post";
+import ProjectPostPreview from "./components/Project/project-post-preview";
 const App: React.FC = () => {
 	return (
 		<Router>
 			<Layouts>
 				<Switch>
-					<Route path="/" exact>
-						<Home />
-					</Route>
-					<Route path="/galleries">
-						<Galleries />
-					</Route>
-					<Route path="/exhibition">
-						<Exhibition />
-					</Route>
-					<Route path="/about">
-						<About />
-					</Route>
-
-					<Route path="/contact">
-						<Contact />
-					</Route>
+					<Route path="/" exact component={Home} />
+					<Route path="/galleries" component={Galleries} />
+					<Route path="/exhibition" component={Exhibition} />
+					<Route path="/project/:slug" component={ProjectPost} />
+					<Route path="/project/" component={ProjectPostPreview} />
+					<Route path="/about" component={About} />
+					<Route path="/contact" component={Contact} />
 				</Switch>
 			</Layouts>
 		</Router>
