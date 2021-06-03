ef49372b72ad3d10512ca950b61bd9be3449cd57
eslamheshamm <esl2mhesham@gmail.com>
2021-06-03 21:44:01 +0200

    adjusting studio strucure

---
+++ studio/deskStructure.js
@@ -0,0 +1,35 @@
+import S from "@sanity/desk-tool/structure-builder";
+import projectPost from "./src/structure/project-post";
+import Galleries from "./src/structure/galleries";
+const hiddenDocTypes = (listItem) =>
+	![
+		"post",
+		"category",
+		"portrait",
+		"documentary",
+		"cinematography",
+		"street",
+		"commercialShots",
+	].includes(listItem.getId());
+
+export default () =>
+	S.list()
+		.title("Base")
+		.items([
+			// S.listItem()
+			// 	.title("Blog")
+			// 	.child(S.document().schemaType("post").documentId("post")),
+			projectPost,
+			Galleries,
+			// S.documentListItem()
+			// 	.schemaType("portrait")
+			// 	.title("portrait")
+			// 	.child(
+			// 		S.editor()
+			// 			.id("portrait")
+			// 			.schemaType("portrait")
+			// 			.documentId("portrait")
+			// 	),
+
+			...S.documentTypeListItems().filter(hiddenDocTypes),
+		]);
---
+++ studio/src/structure/galleries/index.js
@@ -0,0 +1,54 @@
+import S from "@sanity/desk-tool/structure-builder";
+
+const Galleries = S.listItem()
+	.title("Galleries")
+	.child(
+		S.list()
+			.title("Galleries")
+			.items([
+				S.documentListItem()
+					.schemaType("portrait")
+					.title("Portrait")
+					.child(
+						S.editor()
+							.id("portrait")
+							.schemaType("portrait")
+							.documentId("portrait")
+					),
+				S.documentListItem()
+					.schemaType("documentary")
+					.title("Documentary")
+					.child(
+						S.editor()
+							.id("documentary")
+							.schemaType("documentary")
+							.documentId("documentary")
+					),
+				S.documentListItem()
+					.schemaType("cinematography")
+					.title("Cinematography")
+					.child(
+						S.editor()
+							.id("cinematography")
+							.schemaType("cinematography")
+							.documentId("cinematography")
+					),
+				S.documentListItem()
+					.schemaType("street")
+					.title("Street")
+					.child(
+						S.editor().id("street").schemaType("street").documentId("street")
+					),
+				S.documentListItem()
+					.schemaType("commercialShots")
+					.title("Commercial Shots")
+					.child(
+						S.editor()
+							.id("commercialShots")
+							.schemaType("commercialShots")
+							.documentId("commercialShots")
+					),
+			])
+	);
+
+export default Galleries;
