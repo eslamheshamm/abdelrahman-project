c9dcfd1f75b3f4f3fddad2c4d6d76f6b2a16b1fa
eslamheshamm <esl2mhesham@gmail.com>
2021-06-04 23:51:28 +0200

    ramoving filtration

--- studio/src/structure/project-post.js
+++ studio/src/structure/project-post.js
@@ -6,24 +6,6 @@ const projectPost = S.listItem()
 		S.list()
 			.title("Projects Section")
 			.items([
-				S.listItem()
-					.title("Published posts")
-					.schemaType("post")
-					.child(
-						S.documentList("post")
-							.title("Published posts")
-							.menuItems(S.documentTypeList("post").getMenuItems())
-							// Only show posts with publish date earlier than now and that is not drafts
-							.filter(
-								'_type == "post" && publishedAt < now() && !(_id in path("drafts.**"))'
-							)
-							.child((documentId) =>
-								S.document()
-									.documentId(documentId)
-									.schemaType("post")
-									.views([S.view.form()])
-							)
-					),
 				S.documentTypeListItem("post").title("All posts"),
 				S.documentTypeListItem("category").title("Categories"),
 			])
