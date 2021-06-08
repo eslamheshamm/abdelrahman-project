b4d6a2e103c747620a86f5deca74568ee501a21e
eslamheshamm <esl2mhesham@gmail.com>
2021-06-09 00:29:47 +0200

    galleries page

--- frontend/src/pages/galleries/index.tsx
+++ frontend/src/pages/galleries/index.tsx
@@ -1,69 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import portrait from "../../components/assets/portrait.png";
-import documentary from "../../components/assets/documentary.png";
-import cinematography from "../../components/assets/cinematography.png";
-import street from "../../components/assets/street.png";
-import commercialShots from "../../components/assets/commercial-shots.png";
-import { ImageProps, Gallery } from "./types";
+import { galleries } from "./galleries";
+import GalleryImagePreview from "./ImagePreview";
 
-export const GallerImagePreview: React.FC<ImageProps> = (props) => {
-	const { alt, img, title, to } = props;
-	return (
-		<Link to={to}>
-			<figure className="w-full h-[385px] relative text-center cursor-auto">
-				<img
-					src={img}
-					alt={alt}
-					className="w-full h-full object-cover object-center"
-				/>
-				<figcaption className="absolute top-1/2 left-1/2 transform	-translate-y-1/2 -translate-x-2/4	 font-fahkwang uppercase text-white text-4xl md:text-6xl cursor-pointer hover:underline">
-					{title}
-				</figcaption>
-			</figure>
-		</Link>
-	);
-};
-
-const galleries: Gallery[] = [
-	{
-		title: "portrait",
-		id: "portrait",
-		alt: "portrait",
-		img: portrait,
-	},
-	{
-		title: "documentary",
-		id: "documentary",
-		alt: "documentary",
-		img: documentary,
-	},
-
-	{
-		title: "cinematography",
-		id: "cinematography",
-		alt: "cinematography",
-		img: cinematography,
-	},
-	{
-		title: "street",
-		id: "street",
-		alt: "street",
-		img: street,
-	},
-	{
-		title: "commercialShots",
-		id: "commercialShots",
-		alt: "commercial-shots",
-		img: commercialShots,
-	},
-];
 const Galleries: React.FC = () => {
 	return (
 		<section>
 			{galleries.map(({ img, alt, id, title }) => {
 				return (
-					<GallerImagePreview
+					<GalleryImagePreview
 						img={img}
 						title={title}
 						to={`/gallery/${id}`}
