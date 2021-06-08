ba700ffe6b6a504fcdf6b647745f23d330033d2e
eslamheshamm <esl2mhesham@gmail.com>
2021-06-09 00:35:47 +0200

    routing for galleries

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,20 +8,20 @@ import Contact from "./pages/contact";
 import Layouts from "./components/layots";
 import ProjectPost from "./components/Project/project-post";
 import ProjectPostPreview from "./components/Project/project-post-preview";
-import PortraitScreen from "./components/galleries/portrait";
-import { GallerImagePreview } from "./pages/galleries";
+import GalleryScreen from "./components/galleries";
+import GallerImagePreview from "./pages/galleries/ImagePreview";
 const App: React.FC = () => {
 	return (
 		<Router>
 			<Layouts>
 				<Switch>
 					<Route path="/" exact component={Home} />
-					<Route path="/galleries/" component={Galleries} />
-					<Route path="/gallery/" component={GallerImagePreview} />
-					<Route path="/gallery/:id" component={PortraitScreen} />
+					<Route path="/galleries" component={Galleries} />
+					<Route path="/gallery/:id" component={GalleryScreen} />
+					<Route path="/gallery" component={GallerImagePreview} />
 					<Route path="/exhibition" component={Exhibition} />
-					<Route path="/project/" component={ProjectPostPreview} />
 					<Route path="/project/:slug" component={ProjectPost} />
+					<Route path="/project/" component={ProjectPostPreview} />
 					<Route path="/about" component={About} />
 					<Route path="/contact" component={Contact} />
 				</Switch>
