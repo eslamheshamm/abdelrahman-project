c72a3b84474ea3c4848aea4bb36dd4c992eec651
eslamheshamm <esl2mhesham@gmail.com>
2021-06-10 17:52:32 +0200

    building contact page

--- frontend/src/pages/contact/index.tsx
+++ frontend/src/pages/contact/index.tsx
@@ -1,4 +1,14 @@
-const Contact = () => {
-	return <h2>Contact</h2>;
+import React from "react";
+
+const Contact: React.FC = () => {
+	return (
+		<section className="w-11/12 mx-auto">
+			<h2 className="text-4xl md:text-6xl">Contact</h2>
+			<p className="text-sm font-light mt-4">
+				The goal of Verdiv is to provide the same type of <br /> water (taste,
+				quality, price){" "}
+			</p>
+		</section>
+	);
 };
 export default Contact;
